4e38a57ae021f55573ea3728e74a517cc43df624
TEKim <kte9411@gmail.com>
2025-11-24 22:13:55 +0900

    [C#] 251124 - 콘솔 앱 만들기, 최상위 문

---
+++ CSharp/Ch02_TopLevelStatements/Ch02_TopLevelStatements/Program.cs
@@ -0,0 +1,18 @@
+﻿// 최상위 문 (Top-level statements)
+
+// .NET 5 및 이전 버전의 콘솔 앱 템플릿은 Main() 메서드가 명시적으로 필요
+// namespace _02_Top_level_statements
+// {
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Hello, World!");
+//        }
+//    }
+// }
+
+// 최신 콘솔 앱 템플릿은 최상위 문 방식 지원
+// -> Main 메서드 생략 가능
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
